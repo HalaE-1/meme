9ddf244f0ce907523ff2e28347d81d64ce5556be
HalaE_1 <halaezat08_gmail.com>
2026-03-05 20:31:49 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+//https://api.imgflip.com/get_memes
+let btn = document.querySelector("#button");
+let input = document.querySelector("#input");
+let appear = document.querySelector("#mem");
+
+btn.addEventListener("click", () => {
+  let value1 = input.value.trim();
+  let value = Number(value1);
+
+  if (value < 1 || value > 99 || Number.isNaN(value)) {
+    appear.innerHTML = "NOT FOUND !";
+    return;
+  }
+
+  async function get_memes() {
+    try {
+        console.log("clicked");
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json();
+
+      const memes = data.data.memes;    
+      const index = value - 1;          
+      const choose = memes[index];      
+
+      appear.innerHTML = `<img src="${choose.url}" 
+      class="img-fluid rounded mt-4"
+       style="max-width:284px;" height:auto >`;
+    } catch (e) {
+      appear.innerHTML = "ERROR";
+      console.log(e);
+    }
+  }
+
+  get_memes();
+});
